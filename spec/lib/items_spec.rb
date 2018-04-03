947a3dea3c5fd609ff4d678e6c28f15e3df404e7
Truong Nguyen <tmnguyen12@gmail.com>
2018-04-02 19:33:05 -0700

    Trying to add tests

---
+++ spec/lib/items_spec.rb
@@ -0,0 +1,12 @@
+
+describe "ProjectController" do 
+  describe "Get items index of project" do 
+    # project = Project.find_by(id: 13)
+    items = Item.select { |item| item.project_id.to_s == '13'}
+    it "is pulls all items that belong to a specific project" do 
+      expect(items.count).to eql(8)
+    end
+  end 
+end 
+
+
