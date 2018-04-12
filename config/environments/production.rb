f98673acb3951d071241d281f08212452f522b88
Truong Nguyen <tmnguyen12@gmail.com>
2018-04-11 19:19:44 -0700

    update production.rb with CDN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_controller.asset_host = ENV['CDN_HOST']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -34,7 +35,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-
+  
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
