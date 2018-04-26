2536f79c18ee7cd1b4000cf4a4e225d7c8b91ece
Truong Nguyen <tmnguyen12@gmail.com>
2018-04-26 13:46:13 -0700

    removing bundle.js

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-import rootReducer from "../reducers/root_reducer";
+import rootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV !== "production") {
+console.log('I AM IN THE STORE');
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV !== 'production') {
   // must use 'require' (import only allowed at top of file)
-  const { logger } = require("redux-logger");
+  const { logger } = require('redux-logger');
   middlewares.push(logger);
 }
 
