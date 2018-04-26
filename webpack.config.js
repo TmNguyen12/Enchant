d7143b846ff14f541a9df450090bd4f6a828b44a
Truong Nguyen <tmnguyen12@gmail.com>
2018-04-26 16:54:02 -0700

    change item in webpack for uglify

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,12 @@ var prodPlugins = [
     'process.env': {
       NODE_ENV: JSON.stringify('production')
     }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
   })
+  // new webpack.optimize.UglifyJsPlugin({
+  //   compress: {
+  //     warnings: true
+  //   }
+  // })
 ];
 
 plugins = plugins.concat(
